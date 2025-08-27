fb84ca52a9c7639cb1fae893d7ccb922bae0b354
Moritz Twente <127845092+mtwente@users.noreply.github.com>
2025-08-27 11:56:48 +0200

    250825 Data Update, Ballot Topics and Quarto Enhancements (#13)
    * feat: add ballot topics
    
    * feat: update data 20250819
    
    * chore: update git-cliff to v4 in changelog workflow
    
    * fix: fix typo
    
    * feat: split exploratory plots depending on MP origin
    
    * fix: fix MP name typo
    
    * feat: update year entry
    
    * fix: update party affiliations
    
    * feat: make use of dynamically created plots
    
    * feat: add information csv on parties and MPs
    
    * fix: fix citation key
    
    * feat: expand party description
    
    * fix: improve code chunk names
    
    * feat: add substitute MP information
    
    * feat: add more statistics base data
    
    * style: format list alphabetically
    
    * feat: enhance report with better charts
    
    * feat: better linking in quarto
    
    * feat: update codebook to use inline computing
    
    * feat: move static data files to subfolder
    
    * feat: improve markdown formatting
    
    * feat: bring metadata json files up to date
    
    * feat: update party information
    
    * feat: improve code for generating MP table
    
    * feat: enhance codebook with new variables and better formatting
    
    * feat: add party affiliation to MP_dates
    
    * feat: add colours to party information data
    
    * feat: add timeline visualisation of party representation in FT
    
    * fix: improve variable descriptions
    
    * feat: enhance description of dataset
    
    * feat: add Faroese MP L. Petersen to the dataset
    
    * feat: bring targets workflow up to date
    
    * feat: add metadata to ballot_topics
    
    * feat: add missing MP to codebook description
    
    * feat: add UML datamodel visualization
    
    * fix: fix links to renamed data frame
    
    * feat: improve sample plot code
    
    * feat: improve content structure
    
    * chore: remove outdated static files
    
    * feat: restructure page navigation
    
    * feat: improve language
    
    * feat: use lowercase variable names as in other csv files
    
    * ci: snapshot renv lockfile
    
    * ci: add libglpk-dev as dependency to workflow

--- src/create_MP_list.R
+++ src/create_MP_list.R
@@ -15,17 +15,19 @@ MP_names_GL <- data.frame(surname = c("Olsvig", "Lund Olsen", "Jakobsen", "Johan
                          first_name = c("Sara", "Johan", "Doris", "Lars-Emil", "Kuupik", "Sofia", "Juliane", "Nick", "Aaja", "Aleqa", "Aki-Matilda", "Markus E."),
                          MP_id = factor(c(13, 277, 294, 670, 672, 1484, 6689, 14000, 15757, 15758, 18688, 20635)),
                          origin = factor(rep("GL", 12)),
-                         party = factor(c("IA", "IA", "SIU", "SIU", "IA", "IA", "IA", "SIU et al.", "IA", "SIU et al.", "SIU", "SIU")),
+                         party = factor(c("IA", "IA", "SIU", "SIU", "IA", "IA", "IA", "SIU", "IA", "SIU, NQ", "SIU, N", "SIU")),
+                         substitute = c(FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE),
                          start_date = c(rep(NA, 12)),
                          end_date = c(rep(NA, 12)))
 
-MP_names_FO <-  data.frame(surname = c("Joensen", "Arge", "Hoydal", "Johannesen", "Skaale", "á Fríðriksmørk", "Kallsberg", "Falkenberg"),
-                          first_name = c("Edmund", "Magni", "Høgni", "Axel", "Sjúrður", "Annita", "Anfinn", "Anna"),
-                          MP_id = factor(c(247, 15881, 1833, 12283, 262, 6684, 3093, 20349)),
-                          origin = factor(rep("FO", 8)),
-                          party = factor(c("B", "E", "E", "C", "C", "E", "A", "B")),
-                          start_date = c(rep(NA, 8)),
-                          end_date = c(rep(NA, 8)))
+MP_names_FO <-  data.frame(surname = c("Joensen", "Petersen", "Arge", "Hoydal", "Johannesen", "Skaale", "á Fríðriksmørk", "Kallsberg", "Falkenberg"),
+                          first_name = c("Edmund", "Lisbeth", "Magni", "Høgni", "Aksel", "Sjúrður", "Annita", "Anfinn", "Anna"),
+                          MP_id = factor(c(247, 918, 15881, 1833, 12283, 262, 6684, 3093, 20349)),
+                          origin = factor(rep("FO", 9)),
+                          party = factor(c("B", "B", "E", "E", "C", "C", "E", "A", "B")),
+                          substitute = c(FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE),
+                          start_date = c(rep(NA, 9)),
+                          end_date = c(rep(NA, 9)))
 
 MP_names <- rbind(MP_names_GL, MP_names_FO)
 
