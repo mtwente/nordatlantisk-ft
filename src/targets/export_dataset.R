59606b2bd0ef48ae63e68d542e1fd85694f5cd4e
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-10 00:33:19 +0100

    fix: fix file path

--- src/targets/export_dataset.R
+++ src/targets/export_dataset.R
@@ -4,10 +4,10 @@ export_dataset <- function(input_df) {
   
   # Export as CSV and RDS -----
   
-  write.csv(input_df, file = here("data", "processed", "csv", "northatlantic_ft2.csv"),
+  write.csv(input_df, file = here("data", "processed", "csv", "northatlantic_ft.csv"),
             row.names = FALSE)
   
-  saveRDS(input_df, here("data", "processed", "northatlantic_ft2.rds"))
+  saveRDS(input_df, here("data", "processed", "northatlantic_ft.rds"))
   
   return(input_df)
   
