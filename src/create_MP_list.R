9f420c6e13d02f9eafb0665d82fa9893ed2ac470
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-12 01:21:21 +0100

    feat: add voting records for Bjarni Hammer #22

--- src/create_MP_list.R
+++ src/create_MP_list.R
@@ -20,13 +20,13 @@ MP_names_GL <- data.frame(surname = c("Olsvig", "Lund Olsen", "Jakobsen", "Johan
                          substitute = c(FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE)
                          )
 
-MP_names_FO <-  data.frame(surname = c("Joensen", "Petersen", "Arge", "Hoydal", "Johannesen", "Skaale", "á Fríðriksmørk", "Kallsberg", "Falkenberg", "Jacobsen", "Old", "Johannesen", "Rasmussen", "Abrahamsen", "Apol", "Dam"),
-                          first_name = c("Edmund", "Lisbeth", "Magni", "Høgni", "Aksel", "Sjúrður", "Annita", "Anfinn", "Anna", "Tórbjørn", "Henrik", "Kaj Leo", "Magnus", "Helgi", "Barbara Gaardlykke", "Rigmor"),
-                          MP_id = factor(c(247, 918, 15881, 1833, 12283, 262, 6684, 3093, 20349, 9780, 18545, 6687, 19461, 19996, 19142, 18614)),
-                          wikidata_id = factor(c("Q678236", "Q544116", "Q16167420", "Q567780", "Q556712", "Q845278", "Q466097", "Q529900", "Q114967087", "Q331736", "Q3818072", "Q331786", "Q17106707", "Q754881", "Q102501785", "Q15633676")),
-                          origin = factor(rep("FO", 16)),
-                          party = factor(c("B", "B", "E", "E", "C", "C", "E", "A", "B", "E", "C", "B", "B", "B", "C", "C")),
-                          substitute = c(FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)
+MP_names_FO <-  data.frame(surname = c("Joensen", "Petersen", "Arge", "Hoydal", "Johannesen", "Skaale", "á Fríðriksmørk", "Kallsberg", "Falkenberg", "Jacobsen", "Old", "Johannesen", "Rasmussen", "Abrahamsen", "Apol", "Dam", "Hammer"),
+                          first_name = c("Edmund", "Lisbeth", "Magni", "Høgni", "Aksel", "Sjúrður", "Annita", "Anfinn", "Anna", "Tórbjørn", "Henrik", "Kaj Leo", "Magnus", "Helgi", "Barbara Gaardlykke", "Rigmor", "Bjarni"),
+                          MP_id = factor(c(247, 918, 15881, 1833, 12283, 262, 6684, 3093, 20349, 9780, 18545, 6687, 19461, 19996, 19142, 18614, 17988)),
+                          wikidata_id = factor(c("Q678236", "Q544116", "Q16167420", "Q567780", "Q556712", "Q845278", "Q466097", "Q529900", "Q114967087", "Q331736", "Q3818072", "Q331786", "Q17106707", "Q754881", "Q102501785", "Q15633676", "Q20900614")),
+                          origin = factor(rep("FO", 17)),
+                          party = factor(c("B", "B", "E", "E", "C", "C", "E", "A", "B", "E", "C", "B", "B", "B", "C", "C", "C")),
+                          substitute = c(FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)
                           )
 
 MP_names <- rbind(MP_names_GL, MP_names_FO)
