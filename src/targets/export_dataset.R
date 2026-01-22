6f139a54920e3dc517159c2f0bf63d0ee77ead07
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-22 17:38:55 +0100

    feat: move step to order columns to export target

--- src/targets/export_dataset.R
+++ src/targets/export_dataset.R
@@ -2,6 +2,16 @@
 
 export_dataset <- function(input_df) {
   
+  # Clean Up Data Frame Columns  -----
+  
+  col_order <- c("ballot_id", "MP_id", "surname", "origin", "party", "gvt_party_at_home",
+                 "vote_type_id", "vote_id", "ballot_pass", "ft_process_step", "ft_topic_id",
+                 "ft_topic", "ft_for", "ft_against", "ft_abstention", "ft_absent",
+                 "ballot_date", "ballot_type_id", "gvt_type_dk", "gvt_bloc_dk",
+                 "comment", "ballot_result_string")
+  
+  input_df <- input_df[, col_order]
+  
   # Export as CSV and RDS -----
   
   write.csv(input_df, file = here("data", "processed", "csv", "northatlantic_ft.csv"),
--- src/targets/join_results.R
+++ src/targets/join_results.R
@@ -29,16 +29,8 @@ join_results <- function(voting_records, ballot_results, ballot_topics, MP_names
     match_home_government() %>%
     match_danish_government()
   
-  # Clean Up Data Frame Columns  -----
-  
-  col_order <- c("ballot_id", "MP_id", "surname", "origin", "party", "gvt_party_at_home",
-                 "vote_type_id", "vote_id", "ballot_pass", "ft_process_step", "ft_topic_id",
-                 "ft_topic", "ft_for", "ft_against", "ft_abstention", "ft_absent",
-                 "ballot_date", "ballot_type_id", "gvt_type_dk", "gvt_bloc_dk",
-                 "comment", "ballot_result_string")
-  
-  voting_records <- voting_records[, col_order]
-  
+              by = "ft_process_id")
+
   # Return -----
   
   return(voting_records)
