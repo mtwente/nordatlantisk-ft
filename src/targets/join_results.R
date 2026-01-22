66d19f2a16d9b055207ed0369087a84db03b08e4
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-22 17:41:36 +0100

    style: formatting

--- src/targets/join_results.R
+++ src/targets/join_results.R
@@ -9,8 +9,8 @@ join_results <- function(voting_records, ballot_results, ballot_topics, MP_names
   
   voting_records <- voting_records %>%
     left_join(ballot_results %>% select(ballot_id, ballot_pass, ballot_date,
-                                           ft_for, ft_against, ft_abstention, ft_absent,
-                                           ballot_type_id, comment, ballot_result_string, ft_process_id),
+                                                ft_for, ft_against, ft_abstention, ft_absent,
+                                                ballot_type_id, comment, ballot_result_string, ft_process_id),
               by = "ballot_id") %>%
     
     left_join(MP_names %>% select(MP_id, surname, origin),
