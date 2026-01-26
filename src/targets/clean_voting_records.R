e13521cbe1c14f6c707d102e8bc9ceb22ebd13ab
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-26 19:20:42 +0100

    refactor: make variable name easier to understand

--- src/targets/clean_voting_records.R
+++ src/targets/clean_voting_records.R
@@ -5,15 +5,15 @@ library(here)
 
 # Definition -----
 
-clean_voting_records <- function(raw_votes) {
+clean_voting_records <- function(input_df) {
   
   # Manipulate Data -----
   
-  raw_votes <- subset(raw_votes, select = -opdateringsdato)
+  input_df <- subset(input_df, select = -opdateringsdato)
   
-  colnames(raw_votes) <- c("vote_id", "vote_type_id", "ballot_id", "MP_id")
+  colnames(input_df) <- c("vote_id", "vote_type_id", "ballot_id", "MP_id")
   
-  raw_votes <- raw_votes %>%
+  output_df <- input_df %>%
     mutate(vote_id = as.factor(vote_id),
            vote_type_id = as.factor(vote_type_id),
            ballot_id = as.factor(ballot_id),
@@ -22,7 +22,7 @@ clean_voting_records <- function(raw_votes) {
   
   # Export -----
   
-  saveRDS(raw_votes, here("data", "processed", "northatlantic_votes.rds"))
+  saveRDS(output_df, here("data", "processed", "northatlantic_votes.rds"))
   
-  return(raw_votes)
+  return(output_df)
 }
