4e0de9abed8e7549dd19498e87094c352a7934c0
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-26 18:52:20 +0100

    fix: remove voting records from MPs that are no (longer) in MP_names

--- src/targets/get_voting_records.R
+++ src/targets/get_voting_records.R
@@ -9,8 +9,7 @@ source(here("src", "utils", "get_MP_record.R"), local = TRUE)
 # Definition -----
 
 get_voting_records <- function(input_df) {
-  MP_names <- as.data.frame(input_df)
-  
+
   ## read pre-existing dataset to avoid duplicate downloads
   existing_path <- here("data", "raw", "northatlantic_votes_raw.csv")
   
@@ -18,18 +17,27 @@ get_voting_records <- function(input_df) {
     read.csv(existing_path, stringsAsFactors = FALSE)
   } else { NULL }
   
+  
   ## download new voting records only
   new_records <- map_dfr(
-    MP_names$MP_id,
+    input_df$MP_id,
     get_MP_record,
     existing_path = existing_path,
     existing_records = existing_records
   )
   
   ## combine data and sort after MP and vote ID
+  
+  existing_records$opdateringsdato <- as.Date(existing_records$opdateringsdato)
+  new_records$opdateringsdato <- as.Date(new_records$opdateringsdato)
+  
   combined_votes <- bind_rows(existing_records, new_records) %>%
     distinct(id, .keep_all = TRUE) %>%
     arrange(aktørid, id)
+  
+  # Keep only MPs present in input_df
+  combined_votes <- combined_votes %>%
+    filter(aktørid %in% input_df$MP_id)
   
   ## export/return data
   write.csv(combined_votes, file = existing_path, row.names = FALSE)
