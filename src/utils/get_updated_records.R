f6b0aac4f894508838d5afd54d1325f9a04ed38b
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-30 17:45:10 +0100

    fix: get updates also for atomic vectors

--- src/utils/get_updated_records.R
+++ src/utils/get_updated_records.R
@@ -1,9 +1,3 @@
-# Setup -----
-## External Functions -----
-source(here("src", "utils", "get_content.R"))
-
-# Definition -----
-
 get_updated_records <- function(base_url) {
   results <- list()
   skip <- 0
@@ -12,7 +6,19 @@ get_updated_records <- function(base_url) {
     page <- paste0(base_url, "&$skip=", skip) %>%
       get_content()
     
-    values <- page$value
+    # Case 1: Proper OData collection
+    if (is.list(page) && !is.null(page$value)) {
+      values <- page$value
+    }
+    # Case 2: Single object returned
+    else if (is.list(page) && !is.null(page$id)) {
+      values <- list(page)
+    }
+    # Case 3: Nothing usable
+    else {
+      break
+    }
+    
     if (length(values) == 0) break
     
     results <- c(results, values)
