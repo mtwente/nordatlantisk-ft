82a35a01418fe9ae1865e54c3a744bc42e29b74d
mtwente <127845092+mtwente@users.noreply.github.com>
2026-01-31 01:04:05 +0100

    feat: remove ft_process_id variable

--- src/targets/join_results.R
+++ src/targets/join_results.R
@@ -5,8 +5,8 @@ library(here)
 
 # Definition -----
 
-join_results <- function(voting_records, ballot_results, ballot_topics, MP_names) {
-  
+join_results <- function(voting_records, ballot_results, MP_names) {
+    
   voting_records <- voting_records %>%
     left_join(ballot_results %>% select(ballot_id, ballot_pass, ballot_date,
                                                 ft_for, ft_against, ft_abstention, ft_absent,
@@ -14,12 +14,8 @@ join_results <- function(voting_records, ballot_results, ballot_topics, MP_names
               by = "ballot_id") %>%
     
     left_join(MP_names %>% select(MP_id, surname, origin),
-              by = "MP_id") %>%
+              by = "MP_id")
     
-    left_join(ballot_topics %>% select(ft_process_id, ft_process_step,
-                                       ft_topic_id, ft_topic),
-              by = "ft_process_id")
-
   # Return -----
   
   return(voting_records)
