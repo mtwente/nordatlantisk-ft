be18ed290b2e5da22adfebfe45b1fedc4ca77484
mtwente <127845092+mtwente@users.noreply.github.com>
2026-04-26 18:11:39 +0200

    feat: add new B. Johannesen cabinet (FO) #46

--- src/create_cabinet_list_fo.R
+++ src/create_cabinet_list_fo.R
@@ -12,33 +12,33 @@ cabinets_fo <- data.frame(
     "kallsberg_2", "eidesgaard_1", "eidesgaard_2",
     "johannesen-k-l_1-1", "johannesen-k-l_1-2",
     "johannesen-k-l_2-1", "johannesen-k-l_2-2",
-    "johannesen-a_1", "steig-nielsen_1", "johannesen-a_2"
+    "johannesen-a_1", "steig-nielsen_1", "johannesen-a_2", "johannesen-b_1"
   ),
   wikidata_id = factor(c(
     "Q531981", "Q368075", "Q1212499",
     "Q989048", "Q989048", "Q5203949", "Q5203949",
-    "Q20972153", "Q70207330", "Q115858267"
+    "Q20972153", "Q70207330", "Q115858267", "Q139323635"
   )),
   location = factor(c(
-    rep("FO", 10)
+    rep("FO", 11)
   )),
   start = as.Date(c(
     "2002-06-06", "2004-02-03", "2008-02-04",
     "2008-09-26", "2011-04-06", "2011-11-14", "2013-09-09",
-    "2015-09-15", "2019-09-16", "2022-12-22"
+    "2015-09-15", "2019-09-16", "2022-12-22", "2026-04-13"
   )),
   end = as.Date(c(
     "2004-02-03", "2008-02-04", "2008-09-26",
     "2011-04-06", "2011-11-14", "2013-09-09", "2015-09-15",
-    "2019-09-16", "2022-12-22", NA
+    "2019-09-16", "2022-12-22", "2026-04-13", NA
   )),
-  FO_A  = c(TRUE,  TRUE,  FALSE, TRUE,  FALSE, TRUE,  TRUE,  FALSE, TRUE,  FALSE),
-  FO_B  = c(FALSE, TRUE,  FALSE, TRUE,  TRUE,  TRUE,  TRUE,  FALSE, TRUE,  FALSE),
-  FO_C  = c(FALSE, TRUE,  TRUE,  TRUE,  TRUE,  FALSE, FALSE, TRUE,  FALSE, TRUE),
-  FO_D  = c(TRUE,  FALSE, FALSE, FALSE, FALSE, TRUE,  FALSE, FALSE, FALSE, FALSE),
-  FO_E  = c(TRUE,  FALSE, TRUE,  FALSE, FALSE, FALSE, FALSE, TRUE,  FALSE, TRUE),
-  FO_F  = c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE,  FALSE, TRUE),
-  FO_H  = c(TRUE,  FALSE, TRUE,  FALSE, FALSE, TRUE,  TRUE,  FALSE, TRUE,  FALSE),
+  FO_A  = c(TRUE,  TRUE,  FALSE, TRUE,  FALSE, TRUE,  TRUE,  FALSE, TRUE,  FALSE, TRUE),
+  FO_B  = c(FALSE, TRUE,  FALSE, TRUE,  TRUE,  TRUE,  TRUE,  FALSE, TRUE,  FALSE, TRUE),
+  FO_C  = c(FALSE, TRUE,  TRUE,  TRUE,  TRUE,  FALSE, FALSE, TRUE,  FALSE, TRUE, TRUE),
+  FO_D  = c(TRUE,  FALSE, FALSE, FALSE, FALSE, TRUE,  FALSE, FALSE, FALSE, FALSE, FALSE),
+  FO_E  = c(TRUE,  FALSE, TRUE,  FALSE, FALSE, FALSE, FALSE, TRUE,  FALSE, TRUE, FALSE),
+  FO_F  = c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE,  FALSE, TRUE, FALSE),
+  FO_H  = c(TRUE,  FALSE, TRUE,  FALSE, FALSE, TRUE,  TRUE,  FALSE, TRUE,  FALSE, FALSE),
   stringsAsFactors = FALSE
 )
 
